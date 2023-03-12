298323b539a8f14cd11c1bd6a16bb5a1c33a631e
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-12 21:11:00 +0530

    refactor: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
