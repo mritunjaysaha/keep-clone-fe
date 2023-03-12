6bf333eeefcb22f8f0f3982806cf9ea1f65fe31f
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-12 18:05:52 +0530

    feat: set auth token

--- src/core/root.tsx
+++ src/core/root.tsx
@@ -3,6 +3,11 @@ import { Provider } from 'react-redux';
 import { store } from '../redux/store/store';
 import { AppRoutes } from '@/core/routes';
 
+import axios from 'axios';
+
+// @ts-ignore
+axios.defaults.baseURL = process.env.BASE_URL;
+
 export function Root() {
     return (
         <Provider store={store}>
--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -1,8 +1,13 @@
-import { Routes, Route } from 'react-router';
+import { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 import loadable from '@loadable/component';
+import { useDispatch } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 
 import ROUTES from '@/constants/routes.json';
 import { PrivateRoute } from '@/_utils/privateRoute';
+import { setAuthToken } from '@/_utils/setAuthToken';
+import { setAuth } from '@/redux/slices/userSlice';
 
 const Dashboard = loadable(() => import('@/pages/dashboard'));
 
@@ -11,6 +16,28 @@ const Login = loadable(() => import('@/pages/login'));
 const Signup = loadable(() => import('@/pages/signup'));
 
 export function AppRoutes() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (window.localStorage.jwtToken) {
+            const token = localStorage.jwtToken;
+            console.log({ token });
+            setAuthToken(token);
+
+            const decoded = jwt_decode(token);
+            console.log({ decoded });
+
+            dispatch(setAuth(decoded));
+
+            const currentTime = Date.now() / 1000;
+
+            if (decoded.exp < currentTime) {
+                navigate(ROUTES.LOGIN);
+            }
+        }
+    }, []);
+
     return (
         <Routes>
             <Route
