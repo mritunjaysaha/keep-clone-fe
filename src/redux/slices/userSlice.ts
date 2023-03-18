24aedeff54840b80af95f69fadd82fadf5581b11
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-18 17:49:15 +0530

    feat: created store for global states

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const userSlice = createSlice({
-    name: 'auth',
+    name: 'user',
     initialState: {
         isAuthenticated: false,
         _id: '',
