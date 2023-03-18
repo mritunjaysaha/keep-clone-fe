be8fc363e770ed682dc7fc4f84e6465762b19c02
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-18 15:54:13 +0530

    style: Search Input

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,7 +3,7 @@ import { SearchInput } from '@/components/Nav/components/SearchInput';
 
 export function Nav() {
     return (
-        <nav>
+        <nav className='flex items-center justify-between w-screen h-16 border-b-2 border-gray-200'>
             <SearchInput placeholder='Search' />
             <ModeSwitch />
         </nav>
--- src/components/Nav/components/SearchInput.tsx
+++ src/components/Nav/components/SearchInput.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useDeferredValue, useEffect, useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
+import { IoCloseOutline } from 'react-icons/io5';
 
 type SearchInputProps = {
     placeholder: string;
@@ -13,12 +14,22 @@ export function SearchInput({ placeholder }: SearchInputProps) {
         setVal(e.target.value);
     }
 
-    return (
-        <form>
-            <BiSearchAlt2 />
-            <input value={val} placeholder={placeholder} onChange={handleChange} />
+    function handleCloseClick() {
+        console.log('Search close clicked');
+    }
 
-            {deferredValue}
+    return (
+        <form className='flex items-center h-12 bg-gray-600 p-4 rounded-md '>
+            <BiSearchAlt2 size={20} className='mr-2 text-white' />
+            <input
+                className='bg-inherit w-96 pl-4 h-8 placeholder-white text-white'
+                value={val}
+                placeholder={placeholder}
+                onChange={handleChange}
+            />
+            <button className='ml-2' onClick={handleCloseClick}>
+                <IoCloseOutline size={24} className='text-white text-16 cursor-pointer' />
+            </button>
         </form>
     );
 }
