6712d5af89cb8ad735f86e6bf68312a36dfadc40
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-18 17:33:57 +0530

    feat: nav buttons

--- src/components/NavSide/NavSide.tsx
+++ src/components/NavSide/NavSide.tsx
@@ -1,5 +1,40 @@
-export function NavSide() {
-    const isNavSideOpen = false;
+import { NavButtons } from '@/components/NavSide/components/NavButtons';
+import { BsLightbulb } from 'react-icons/bs';
 
-    return <section className={`${isNavSideOpen ? 'w-12' : 'w-60'} bg-red-500 h-full`}></section>;
+export function NavSide() {
+    const isNavSideClose = false;
+
+    return (
+        <section className={`flex flex-col ${isNavSideClose ? 'w-16' : 'w-60'} bg-red-500 h-full`}>
+            <NavButtons
+                icon={BsLightbulb}
+                label='notes'
+                isActive={true}
+                isNavSideClose={isNavSideClose}
+                onClick={() => {
+                    console.log('notes clicked');
+                }}
+            />
+
+            <NavButtons
+                icon={BsLightbulb}
+                label='notes'
+                isActive={false}
+                isNavSideClose={isNavSideClose}
+                onClick={() => {
+                    console.log('notes clicked');
+                }}
+            />
+
+            <NavButtons
+                icon={BsLightbulb}
+                label='notes'
+                isActive={false}
+                isNavSideClose={isNavSideClose}
+                onClick={() => {
+                    console.log('notes clicked');
+                }}
+            />
+        </section>
+    );
 }
--- src/components/NavSide/components/NavButtons.tsx
+++ src/components/NavSide/components/NavButtons.tsx
@@ -2,21 +2,30 @@ type NavButtonProps = {
     icon: any;
     label: string;
     isActive: boolean;
-    isNavSideOpen: boolean;
+    isNavSideClose: boolean;
     onClick: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 export function NavButtons({
     icon: Icon,
     label,
-    isNavSideOpen,
+    isNavSideClose,
     isActive,
     onClick,
 }: NavButtonProps) {
     return (
-        <button onClick={onClick} className=''>
-            <Icon />
-            <span className={`${isNavSideOpen ? '' : 'hidden'}`}>{label}</span>
+        <button
+            onClick={onClick}
+            className={`flex items-center ${
+                isNavSideClose ? 'justify-center' : 'justify-between'
+            } ${
+                isActive ? 'bg-yellow-300' : ''
+            } h-12 pl-4 border-1 border-transparent rounded-r-full rounded-l-none hover:bg-fuchsia-300 hover:border-blue`}
+        >
+            <Icon size={32} />
+            <span className={`${isNavSideClose ? 'hidden' : 'w-full'} text-left ml-8 text-lg`}>
+                {label}
+            </span>
         </button>
     );
 }
